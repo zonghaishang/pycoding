fdd660acab708445b7f313f586985760602dfc56
zonghai.szh <zonghai.szh@antgroup.com>
2025-07-09 20:01:46 +0800

    learn chap3

---
+++ 03-dict-set/map.py
@@ -0,0 +1,23 @@
+
+from collections.abc import Mapping, MutableMapping
+
+mydict = {}
+
+print('isinstance(mydict, Mapping)', isinstance(mydict, Mapping))
+print('isinstance(mydict, MutableMapping)', isinstance(mydict, MutableMapping))
+
+tt = (1,2,(3,4))
+print(hash(tt))
+
+
+test = [1,2,3,4,5,6]
+dd: dict = mydict.fromkeys(test, -1)
+print(mydict)
+print(dd)
+
+r=dd.__getitem__(1)
+print('dd.__getitem__(1)', r)
+
+r=dd.get(7, 7)
+print('dd.get(7, __default=7)', r)
+
