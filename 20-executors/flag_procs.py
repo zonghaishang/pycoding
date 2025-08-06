79783f6c3bffc1d2de669d5855ed629b89405832
zonghaishang <yiji@apache.org>
2025-08-06 19:49:04 +0800

    learn chap20

---
+++ 20-executors/flag_procs.py
@@ -0,0 +1,20 @@
+from concurrent import futures
+
+from flags_single import save_flag, get_flag, main
+
+
+def download_one(cc: str):
+    image = get_flag(cc)
+    save_flag(image, f'{cc}.gif')
+    print(cc, end=' ', flush=True)
+    return cc
+
+
+def download_many(cc_list: list[str]) -> int:
+    with futures.ProcessPoolExecutor() as exe:
+        res = exe.map(download_one, sorted(cc_list))
+    return len(list(res))
+
+
+if __name__ == '__main__':
+    main(download_many)
